bfa2b2ffd8d5a131b718ebbc9c57d57ccddc902f
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-02-20 15:42:18 +0100

    relaciones entre tablas p3 terminadas

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Matias Aguila',
+            'email' => 'matias@test.com',
+        ]);
+    }
+}

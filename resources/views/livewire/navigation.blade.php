52ed9ffde59d496dd94316e95695b80a3304e788
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-02-28 00:53:00 +0100

    video 19
    En este paso añadimos la librería font.. además de añadir los link de login y registro

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -13,9 +13,10 @@ Categorías
         <x-jet-application-mark class="block h-9 w-auto" />
         </a>
         @livewire('search')
-        @auth
+
 
         <div class="ml-3 relative">
+            @auth
             <x-jet-dropdown align="right" width="48">
                 <x-slot name="trigger">
                         <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
@@ -48,6 +49,26 @@ Categorías
                     </form>
                 </x-slot>
             </x-jet-dropdown>
+
+            @else
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                    </x-slot>
+                    <x-slot name="content">
+                        <x-jet-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-jet-dropdown-link>
+                        <x-jet-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-jet-dropdown-link>
+
+
+
+                    </x-slot>
+                </x-jet-dropdown>
+
+
             @endauth
         </div>
 

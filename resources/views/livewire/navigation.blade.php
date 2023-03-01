dcf3ad58e802661375dc8c343221ae1e259cc8c3
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-01 21:17:33 +0100

    modificación vista movil
    La vista en las ventanas pequeñas cerrado

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -7,6 +7,7 @@
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4
                 12h16M4 18h16" />
             </svg>
+
             <span class="text-sm">
                 Categorías
             </span>
@@ -14,10 +15,13 @@
         <a href="/" class="mx-6">
         <x-jet-application-mark class="block h-9 w-auto" />
         </a>
-        @livewire('search')
+
+       <div class="flex-1 hidden md:block">
+           @livewire('search')
+       </div>
 
 
-        <div class="mx-6 relative">
+        <div class="mx-6 relative hidden md:block">
             @auth
             <x-jet-dropdown align="right" width="48">
                 <x-slot name="trigger">
@@ -74,7 +78,9 @@
             @endauth
         </div>
 
+    <div class="hidden md:block">
         @livewire('dropdown-cart')
+    </div>
 
     </div>
     <nav id="navigation-menu"

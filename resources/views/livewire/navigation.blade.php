26813ce9ea4e8d6c20e6d14299dbb80243e2d2c9
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-01 20:17:35 +0100

    Libreria alpine
    estamos usando la librería alpine, js la cual nos permite dar funcionalidad a nuestro despliegue de categorias y subcategorias
    ver el codigo <b>es importante</b>

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -6,9 +6,11 @@
         display: block !important;
     }
 </style>
-<header class="bg-trueGray-700 sticky top-0">
+<header class="bg-trueGray-700 sticky top-0" x-data="{ open: false }">
     <div class="container-menu flex items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a
+            x-on:click="open = !open"
+                class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4
 12h16M4 18h16" />
@@ -83,9 +85,12 @@ Categorías
         @livewire('dropdown-cart')
 
     </div>
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu"
+         x-show="open"
+         :class="{'block': open, 'hidden': !open}"
+         class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
         <div class="container-menu h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            <div  x-on:click.away="open = false"  class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach($categories as $category)
                         <li class="navigation-link hover:bg-orange-500 hover:text-white">

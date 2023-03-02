24db9a31270d2f7bcefadc48a504a4517f9e4c3f
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-02 12:23:11 +0100

    glider
    glider a medio funcionar 1 categoria

--- resources/views/livewire/category-products.blade.php
+++ resources/views/livewire/category-products.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <div class="glider-contain">
+
+        @foreach($category->products as $product)
+            <li class="bg-white rounded-lg shadow {{ ! $loop->last ? 'mr-4' : '' }}">
+                <figure>
+                    <img src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                </figure>
+            </li>
+        @endforeach
+        <button aria-label="Previous" class="glider-prev">«</button>
+        <button aria-label="Next" class="glider-next">»</button>
+        <div role="tablist" class="dots"></div>
+    </div>
 </div>

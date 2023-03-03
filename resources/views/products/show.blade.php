95eaed25ddfa331f528d98266211d0006d3d2a38
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-04 00:19:17 +0100

    imagenes funcionando

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,10 +1,28 @@
 <x-app-layout>
-
-
-
-
-
-
-
+    <div class="container-menu py-8">
+        <div class="grid grid-cols-2 gap-6">
+            <div>
+                <div class="flexslider">
+                    <ul class="slides">
+                        @foreach($product->images as $image)
+                            <li data-thumb="{{ Storage::url($image->url) }}">
+                                <img src="{{ Storage::url($image->url) }}" />
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    controlNav: "thumbnails"
+                });
+            });
+        </script>
+    @endpush
 
 </x-app-layout>

daf36ea71f6dfa101ea00cbc8a8dedee684eda7a
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-03 13:26:15 +0100

    usar paginador

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -3,6 +3,9 @@
         <figure class="mb-4">
             <img class="w-full h-80 object-cover object-center" src="{{ Storage::url($category->image) }}" alt="">
         </figure>
+
+        @livewire('category-filter', ['category' => $category])
+
     </div>
 
 </x-app-layout>

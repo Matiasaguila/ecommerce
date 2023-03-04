3f40e4cbd6f6ed6de84dcf4afb88a85199ccc427
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-04 01:02:23 +0100

    add card item
    add car item máximos y mínimos
    
    video 48

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 class AddCartItem extends Component
 {
     public $qty = 1;
+    public $product;
+    public $quantity;
+    public function mount()
+    {
+        $this->quantity = $this->product->quantity;
+    }
     public function decrement()
     {
         $this->qty--;
--- resources/views/livewire/add-cart-item.blade.php
+++ resources/views/livewire/add-cart-item.blade.php
@@ -1,4 +1,7 @@
 <div x-data>
+    <p class="text-gray-700 mb-4">
+        <span class="font-semibold text-lg">Stock disponible:</span> {{$quantity}}
+    </p>
     <div class="flex">
         <div class="mr-4">
             <x-jet-secondary-button disabled
@@ -9,7 +12,11 @@
                 -
             </x-jet-secondary-button>
             <span class="mx-2 text-gray-700">{{ $qty }}</span>
-            <x-jet-secondary-button wire:click="increment">
+            <x-jet-secondary-button
+                    x-bind:disabled="$wire.qty >= $wire.quantity"
+                    wire:loading.attr="disabled"
+                    wire:target="increment"
+                    wire:click="increment">
                 +
             </x-jet-secondary-button>
         </div>

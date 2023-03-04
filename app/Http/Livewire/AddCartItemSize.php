d9c6dee101d2800faac8baf8e32f6d4ccd6931ae
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-04 19:48:53 +0100

    añadir productos a medio funcionar

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -9,9 +9,12 @@ class AddCartItemSize extends Component
 {
 
     public $product;
+    public $color_id = '';
     public $sizes;
     public $size_id = '';
     public $colors = [];
+    public $qty = 1;
+    public $quantity = 0;
     public function mount()
     {
         $this->sizes = $this->product->sizes;
@@ -21,7 +24,19 @@ class AddCartItemSize extends Component
         $size = Size::find($value);
         $this->colors = $size->colors;
     }
-
+    public function updatedColorId($value)
+    {
+        $size = Size::find($this->size_id);
+        $this->quantity = $size->colors->find($value)->pivot->quantity;
+    }
+    public function decrement()
+    {
+        $this->qty--;
+    }
+    public function increment()
+    {
+        $this->qty++;
+    }
     public function render()
     {
         return view('livewire.add-cart-item-size');

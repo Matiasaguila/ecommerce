d2ad7869459aa82e58c9a4779c49a569bea2c873
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-04 19:12:34 +0100

    traducion a otro idioma
    traduccion a español

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -3,7 +3,7 @@
     <select wire:model="color_id" class="form-control w-full">
         <option value="" selected disabled>Seleccionar un color</option>
         @foreach ($colors as $color)
-            <option value="{{$color->id}}">{{ $color->name }}</option>
+            <option value="{{$color->id}}">{{ __(ucfirst($color->name))  }}</option>
         @endforeach
     </select>
     <div class="flex mt-4">

2f5126282d7ce37757b864461401dea1fb8b4422
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-04 01:08:59 +0100

    video 49

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -1,3 +1,9 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div x-data>
+    <p class="text-xl text-gray-700">Color:</p>
+    <select class="form-control w-full">
+        <option value="" selected disabled>Seleccionar un color</option>
+        @foreach ($colors as $color)
+            <option value="{{$color->id}}">{{ $color->name }}</option>
+        @endforeach
+    </select>
 </div>

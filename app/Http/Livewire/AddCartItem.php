7b4b62e1ca25e8445029511f1bc9891512d1b60b
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-05 11:09:02 +0100

    add card itm 1 funcionando

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -38,6 +38,7 @@ class AddCartItem extends Component
             'weight' => 550,
             'options' => $this->options,
         ]);
+        $this->emitTo('dropdown-cart', 'render');
     }
 
         public function render()

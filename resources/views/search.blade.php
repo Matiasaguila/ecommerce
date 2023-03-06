1b937bc836357d0abe5f01d936544772af7260e8
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-06 14:17:58 +0100

    mostar resultados de busqueda 1

---
+++ resources/views/search.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <ul>
+        @foreach($products as $product)
+            <li>{{ $product->name }}</li>
+        @endforeach
+    </ul>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SearchController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +21,8 @@ Route::get('/', WelcomeController::class);
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+
+Route::get('search', SearchController::class)->name('search');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');

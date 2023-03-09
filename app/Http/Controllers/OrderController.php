79f9d372a847652553fdd043e0ee45e599c16d69
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-09 23:21:38 +0100

    detalles de la ordenes
    video 110

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,13 +9,9 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
-    public function payment(Order $order)
-    {
-        $items = json_decode($order->content);
-        return view('orders.payment', compact('order', 'items'));
-    }
+
     public function show(Order $order)
     {
-        return view('orders.show', compact('order'));
+        return view('orders.show', compact('order', 'items'));
     }
 }
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,3 +1,50 @@
 <x-app-layout>
 
+    <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+
+
+        <div class="bg-white rounded-lg shadow-lg px-12 py-8 mb-6 flex items-center">
+
+            <div class="relative">
+                <div class="{{ ($order->status >= 2 && $order->status != 5) ? 'bg-blue-400' : 'bg-gray-400' }}  rounded-full h-12 w-12 flex items-center justify-center">
+                    <i class="fas fa-check text-white"></i>
+                </div>
+
+                <div class="absolute -left-1.5 mt-0.5">
+                    <p>Recibido</p>
+                </div>
+            </div>
+
+            <div class="{{ ($order->status >= 3 && $order->status != 5) ? 'bg-blue-400' : 'bg-gray-400' }} h-1 flex-1 mx-2"></div>
+
+            <div class="relative">
+                <div class="{{ ($order->status >= 3 && $order->status != 5) ? 'bg-blue-400' : 'bg-gray-400' }} rounded-full h-12 w-12 flex items-center justify-center">
+                    <i class="fas fa-truck text-white"></i>
+                </div>
+
+                <div class="absolute -left-1 mt-0.5">
+                    <p>Enviado</p>
+                </div>
+            </div>
+
+            <div class="{{ ($order->status >= 4 && $order->status != 5) ? 'bg-blue-400' : 'bg-gray-400' }} h-1 flex-1 mx-2"></div>
+
+            <div class="relative">
+                <div class="{{ ($order->status >= 4 && $order->status != 5) ? 'bg-blue-400' : 'bg-gray-400' }} rounded-full h-12 w-12 flex items-center justify-center">
+                    <i class="fas fa-check text-white"></i>
+                </div>
+
+                <div class="absolute -left-2 mt-0.5">
+                    <p>Entregado</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
 </x-app-layout>

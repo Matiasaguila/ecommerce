19fec72addce50a867c24caa4233f75006261dc3
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-09 01:36:10 +0100

    fin video 91
    mejora vista de ordenes

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -3,9 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Department;
 
 class CreateOrder extends Component
 {
+    public $districts =[], $cities=[];
+    public $departments, $address,$reference ;
+    public $department_id = '', $city_id = '', $district_id = '';
+
+    public function mount()
+    {
+        $this->departments = Department::all();
+    }
+
     public function render()
     {
         return view('livewire.create-order');

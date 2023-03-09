a8bee4b9499a2bbb521d2b7968ff16adde5bd5e9
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-09 17:08:53 +0100

    crear ordenes modificado y funcionando

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -1,74 +1,100 @@
 <div class="container-menu py-8 grid grid-cols-5 gap-6">
     <div class="col-span-3">
+
         <div class="bg-white rounded-lg shadow p-6">
             <div class="mb-4">
                 <x-jet-label value="Nombre de contacto" />
                 <x-jet-input type="text"
+                             wire:model.defer="contact"
                              placeholder="Introduzca el nombre de la persona que recibirá el pedido"
                              class="w-full"/>
+                <x-jet-input-error for="contact" />
             </div>
             <div>
                 <x-jet-label value="Teléfono de contacto" />
                 <x-jet-input type="text"
+                             wire:model.defer="phone"
                              placeholder="Introduzca el teléfono de contacto"
                              class="w-full"/>
+                <x-jet-input-error for="phone" />
             </div>
         </div>
+
         <div x-data="{ envio_type: @entangle('envio_type')}">
             <p class="mt-6 mb-3 text-lg text-gray-700 font-semibold">Envíos</p>
-            <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center mb-4">
+
+            <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center mb-4 cursor-pointer">
                 <input x-model="envio_type" type="radio" name="envio_type" value="1" class="text-gray-600">
                 <span class="ml-2 text-gray-700">Recojo en tienda (Calle Falsa 123)</span>
+
                 <span class="font-semibold text-gray-700 ml-auto">Gratis</span>
             </label>
+
             <div class="bg-white rounded-lg shadow">
-            <label class="px-6 py-4 flex items-center">
-                <input x-model="envio_type" type="radio" name="envio_type" value="2" class="text-gray-600">
-                <span class="ml-2 text-gray-700">Envío a domicilio</span>
-            </label>
+                 <label class="px-6 py-4 flex items-center cursor-pointer">
+                        <input x-model="envio_type" type="radio" name="envio_type" value="2" class="text-gray-600">
+                        <span class="ml-2 text-gray-700">Envío a domicilio</span>
+                 </label>
 
                 <div class="px-6 pb-6 grid grid-cols-2 gap-6" :class="{ 'hidden': envio_type != 2 }">
                     <div>
-                        <x-jet-label value="Departamento" />
+                        <x-jet-label value="Departamento"/>
+
+
                         <select class="form-control w-full" wire:model="department_id">
                             <option value="" disabled selected>Seleccione un departamento</option>
                             @foreach($departments as $department)
                                 <option value="{{ $department->id }}">{{ $department->name }}</option>
                             @endforeach
                         </select>
+
+                        <x-jet-input-error for="department_id" />
                     </div>
+
                     <div>
-                        <x-jet-label value="Ciudad" />
+                        <x-jet-label value="Ciudad"/>
+
                         <select class="form-control w-full" wire:model="city_id">
                             <option value="" disabled selected>Seleccione una ciudad</option>
                             @foreach($cities as $city)
                                 <option value="{{ $city->id }}">{{ $city->name }}</option>
                             @endforeach
                         </select>
+
+                        <x-jet-input-error for="city_id" />
                     </div>
 
                     <div>
                         <x-jet-label value="Distrito" />
+
                         <select class="form-control w-full" wire:model="district_id">
                             <option value="" disabled selected>Seleccione un distrito</option>
                             @foreach($districts as $district)
-                                <option value="{{ $district->id }}">{{ $district->name }}</option>
+
+                            <option value="{{ $district->id }}">{{ $district->name }}</option>
                             @endforeach
                         </select>
+                        <x-jet-input-error for="district_id" />
                     </div>
                     <div>
                         <x-jet-label value="Dirección" />
                         <x-jet-input class="w-full" wire:model="address" type="text" />
+                        <x-jet-input-error for="address" />
                     </div>
                     <div class="col-span-2">
                         <x-jet-label value="Referencia" />
                         <x-jet-input class="w-full" wire:model="reference" type="text" />
+                        <x-jet-input-error for="reference" />
                     </div>
                 </div>
             </div>
         </div>
+
         <div>
-            <x-jet-button class="mt-6 mb-4">
+            <x-jet-button class="mt-6 mb-4"
+                          wire.loading.attr="disabled"
+                          wire:target="create_order"
+                          wire:click="create_order">
                 Continuar con la compra
             </x-jet-button>
             <hr>

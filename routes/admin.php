32d7b3c75c600db9d950de8c9c409d1daf2ac686
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-11 19:47:32 +0100

    fin video 125
    vista crear productos

--- routes/admin.php
+++ routes/admin.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\livewire\Admin\ShowProducts;
+use App\Http\livewire\Admin\CreateProduct;
 
 
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
-Route::get('products/create', function () {})->name('admin.products.create');
+Route::get('products/create', CreateProduct::class)->name('admin.products.create');

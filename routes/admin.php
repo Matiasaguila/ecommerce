a10221649c84f83cb3458e182e2d3166d280ff7a
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-11 19:22:10 +0100

    fin video 123
    Mostramos los productos en el listado con logica

--- routes/admin.php
+++ routes/admin.php
@@ -6,3 +6,5 @@ use App\Http\livewire\Admin\ShowProducts;
 
 
 Route::get('/', ShowProducts::class)->name('admin.index');
+Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
+Route::get('products/create', function () {})->name('admin.products.create');

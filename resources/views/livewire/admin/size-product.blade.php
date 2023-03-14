945b3521fc7b53cc416dc6704c869a736c4f2929
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-14 18:55:00 +0100

    fin video 142
    Agregamos parte del diseño de agregar tallas

--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -1,2 +1,33 @@
 <div>
-    Necesita especificar una talla y un color</div>
+
+        <div class="bg-white shadow-lg rounded-lg p-6 mt-12">
+            <div>
+                <x-jet-label>
+                    Cantidad
+                </x-jet-label>
+                <x-jet-input
+                        wire:model="name"
+                        type="text"
+                        placeholder="Introduzca una talla"
+                        class="w-full"/>
+                <x-jet-input-error for="name" />
+            </div>
+            <div class="flex justify-end items-center mt-4">
+                <x-jet-button wire:click="save" wire:loading.attr="disabled" wire:target="save">
+                    Agregar
+                </x-jet-button>
+            </div>
+        </div>
+    <ul class="mt-12 space-y4">
+        @foreach ($sizes as $size)
+            <li class="bg-white shadow-lg rounded-lg p-6" wire:key="size-{{ $size->id }}">
+                <div class="flex items-center">
+                    <span class="text-xl font-medium">{{ $size->name }}</span>
+                </div>
+            </li>
+            @endforeach
+
+
+    </ul>
+
+</div>

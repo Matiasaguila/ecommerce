adde5a35a31ab6af9a6ffd8074c9af4da7920969
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-14 19:05:58 +0100

    fin video 143
    actualizacion de productos con talla y color semifuncional

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -3,10 +3,14 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Size;
 
 class SizeProduct extends Component
 {
-    public $product,$name;
+    public $product,$name,$size;
+    public $name_edit;
+
+    public $open = false;
     protected $rules = [
         'name' => 'required'
     ];
@@ -19,9 +23,25 @@ class SizeProduct extends Component
         ]);
         $this->product = $this->product->fresh();
     }
+    public function edit(Size $size)
+    {
+        $this->open = true;
+        $this->size = $size;
+        $this->name_edit = $size->name;
+    }
     public function render()
     {
         $sizes = $this->product->sizes;
         return view('livewire.admin.size-product',compact('sizes'));
+    }
+    public function update()
+    {
+        $this->validate([
+            'name_edit' => 'required'
+        ]);
+        $this->size->name = $this->name_edit;
+        $this->size->save();
+        $this->product = $this->product->fresh();
+        $this->open = false;
     }
 }

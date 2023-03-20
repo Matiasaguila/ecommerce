8eb5e8a3ecf596e07de8430bced36ad5dc0bdb0b
Matiasaguila <113832316+Matiasaguila@users.noreply.github.com>
2023-03-20 17:42:37 +0100

    179
    video 179

--- app/Http/Livewire/PaymentOrder.php
+++ app/Http/Livewire/PaymentOrder.php
@@ -29,9 +29,11 @@ class PaymentOrder extends Component
     {
 
         $this->authorize('view', $this->order);
-      $this->authorize('payment', $this->order);
+
         $items = json_decode($this->order->content);
-        return view('livewire.payment-order', compact('items'));
+
+        $envio = json_decode($this->order->envio)   ;
+        return view('livewire.payment-order', compact('items','envio'));
     }
 
 }
